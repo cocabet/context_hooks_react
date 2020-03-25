a00a015b31d5e0eab59c04314791c59d01ed447d
Oscar <cocabet@gmail.com>
2020-03-24 23:26:52 -0600

    Se creo la carpeta contexts, y el ThemeContext

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -1,13 +1,18 @@
 import React, { Component } from 'react';
+import { ThemeContext } from '../contexts/ThemeContext';
 
 class BookList extends Component {
+  static contextType = ThemeContext;
   render() { 
+    const {isLightTheme, light, dark } = this.context;
+    const theme = isLightTheme ? light : dark;
+
     return(
-      <div className="book-list">
+      <div className="book-list" style={{background: theme.bg, color: theme.syntax}}>
         <ul>
-          <li>The Way of KIngs</li>
-          <li>The Name of the Wing</li>
-          <li>The Final Empire</li>
+          <li style={{ background: theme.ui}} >The Way of KIngs</li>
+          <li style={{ background: theme.ui}}>The Name of the Wing</li>
+          <li style={{ background: theme.ui}}>The Final Empire</li>
         </ul>
       </div>
     );
